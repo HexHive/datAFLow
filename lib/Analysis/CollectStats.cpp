c65d651c9ca8d7fa44f45dbdea712373a9add71f
Adrian Herrera <adrian.herrera02@gmail.com>
2022-08-14 17:24:33 +1000

    add dataflow-stats tool

---
+++ lib/Analysis/CollectStats.cpp
@@ -0,0 +1,95 @@
+//===-- CollectStats.cpp - Collect useful statistics ------------*- C++ -*-===//
+///
+/// \file
+/// This pass simply collects a number of useful statistics.
+///
+//===----------------------------------------------------------------------===//
+
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/Module.h>
+#include <llvm/Pass.h>
+#include <llvm/Transforms/IPO/PassManagerBuilder.h>
+
+#include "fuzzalloc/Analysis/CollectStats.h"
+#include "fuzzalloc/Metadata.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "fuzzalloc-collect-stats"
+
+char CollectStats::ID = 0;
+
+void CollectStats::getAnalysisUsage(AnalysisUsage &AU) const {
+  AU.setPreservesAll();
+}
+
+void CollectStats::print(raw_ostream &O, const Module *) const {
+  O << "  num. basic blocks: " << this->NumBasicBlocks << "\n";
+  O << "  num. allocas: " << this->NumAllocas << "\n";
+  O << "  num. global variables: " << this->NumGlobalVars << "\n";
+  O << "  num. tagged allocs: " << this->NumTaggedAllocs << "\n";
+  O << "  num. of dereferenced pointers: " << this->NumInstrumentedDerefs
+    << "\n";
+  O << "  num. of heapified allocas: " << this->NumHeapifiedAllocas << "\n";
+}
+
+bool CollectStats::doInitialization(Module &M) {
+  this->NumBasicBlocks = 0;
+  this->NumAllocas = 0;
+  this->NumGlobalVars = 0;
+  this->NumTaggedAllocs = 0;
+  this->NumInstrumentedDerefs = 0;
+  this->NumHeapifiedAllocas = 0;
+
+  return false;
+}
+
+bool CollectStats::runOnModule(Module &M) {
+  for (const auto &F : M.functions()) {
+    for (auto &BB : F) {
+      this->NumBasicBlocks++;
+
+      for (auto &I : BB) {
+        if (isa<AllocaInst>(I)) {
+          this->NumAllocas++;
+        }
+
+        if (I.getMetadata(M.getMDKindID(kFuzzallocTaggedAllocMD))) {
+          this->NumTaggedAllocs++;
+        } else if (I.getMetadata(
+                       M.getMDKindID(kFuzzallocInstrumentedDerefMD))) {
+          this->NumInstrumentedDerefs++;
+        } else if (I.getMetadata(M.getMDKindID(kFuzzallocHeapifiedAllocaMD))) {
+          this->NumHeapifiedAllocas++;
+        }
+      }
+    }
+  }
+
+  for (const auto &G : M.globals()) {
+    if (auto *GV = dyn_cast<GlobalVariable>(&G)) {
+      if (!GV->isDeclaration()) {
+        this->NumGlobalVars++;
+      }
+    }
+  }
+
+  return false;
+}
+
+static RegisterPass<CollectStats>
+    X(DEBUG_TYPE, "Collect some useful statistics", true, true);
+
+static void registerCollectStatsPass(const PassManagerBuilder &,
+                                     legacy::PassManagerBase &PM) {
+  PM.add(new CollectStats());
+}
+
+static RegisterStandardPasses
+    RegisterCollectStatsPass(PassManagerBuilder::EP_ModuleOptimizerEarly,
+                             registerCollectStatsPass);
+
+static RegisterStandardPasses
+    RegisterCollectStatsPass0(PassManagerBuilder::EP_EnabledOnOptLevel0,
+                              registerCollectStatsPass);
---
+++ tools/dataflow-stats.cpp
@@ -0,0 +1,60 @@
+//===-- dataflow-stats.cpp - Collect useful statistics ----------*- C++ -*-===//
+///
+/// \file
+/// Get some datAFLow stats
+///
+//===----------------------------------------------------------------------===//
+
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Pass.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/WithColor.h>
+
+#include "fuzzalloc/Analysis/CollectStats.h"
+#include "fuzzalloc/Streams.h"
+
+using namespace llvm;
+
+namespace {
+//
+// Command-line options
+//
+
+static cl::OptionCategory Cat("Collect fuzzalloc statistics");
+static cl::opt<std::string> BCFilename(cl::Positional, cl::desc("<BC file>"),
+                                       cl::value_desc("path"), cl::Required,
+                                       cl::cat(Cat));
+} // anonymous namespace
+
+int main(int argc, char *argv[]) {
+  cl::HideUnrelatedOptions(Cat);
+  cl::ParseCommandLineOptions(argc, argv, "Collect fuzzalloc statistics");
+
+  // Parse bitcode
+  status_stream() << "Parsing " << BCFilename << "...\n";
+  LLVMContext Ctx;
+  SMDiagnostic Err;
+  auto Mod = parseIRFile(BCFilename, Err, Ctx);
+  if (!Mod) {
+    error_stream() << "Failed to parse `" << BCFilename
+                   << "`: " << Err.getMessage() << '\n';
+    ::exit(1);
+  }
+
+  // Run pass
+  status_stream() << "Collecting stats...\n";
+  legacy::PassManager PM;
+  auto *GetStats = new CollectStats;
+  PM.add(GetStats);
+  PM.run(*Mod);
+
+  GetStats->print(errs(), Mod.get());
+
+  // Cleanup
+  llvm_shutdown();
+
+  return 0;
+}
