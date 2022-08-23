292039766a81c83803a96320badd8500a3d34516
Adrian Herrera <adrian.herrera02@gmail.com>
2022-08-23 20:27:37 +1000

    switched back to old hash routine

--- lib/Runtime/Hash.c
+++ lib/Runtime/Hash.c
@@ -13,29 +13,28 @@
 
 // AFL++ headers
 #include "config.h"
-#define XXH_INLINE_ALL
-#include "xxhash.h"
-#undef XXH_INLINE_ALL
 
 #define likely(x) __builtin_expect((x), 1)
 #define unlikely(x) __builtin_expect((x), 0)
 
 #if (MAP_SIZE_POW2 <= 16)
-#define HASH_MASK (0xFFFF)
+typedef uint16_t cov_idx_t;
+#define PRIxHash PRIx16
 #elif (MAP_SIZE_POW2 <= 32)
-#define HASH_MASK (0xFFFFFFFF)
+typedef uint32_t cov_idx_t;
+#define PRIxHash PRIx32
 #else
-#define HASH_MASK (0xFFFFFFFFFFFFFFFF)
+typedef uint64_t cov_idx_t;
+#define PRIxHash PRIx64
 #endif
 
-static const XXH64_hash_t kSeed = 0;
 extern uint8_t *__afl_area_ptr;
 
 /// Update AFL coverage bitmap
-static inline void __afl_update_cov(XXH64_hash_t Idx) {
-  uint8_t *P = &__afl_area_ptr[Idx & HASH_MASK];
+static inline void __afl_update_cov(cov_idx_t Idx) {
+  uint8_t *P = &__afl_area_ptr[Idx];
 #if __GNUC__
-  uint8_t C = __builtin_add_overflow(*P, 1, P);
+  const uint8_t C = __builtin_add_overflow(*P, 1, P);
   *P += C;
 #else
   *P += 1 + ((uint8_t)(1 + *P) == 0);
@@ -49,18 +48,18 @@ static inline void __afl_update_cov(XXH64_hash_t Idx) {
 void __afl_hash_def_use(void *Ptr, size_t Size) {
   uintptr_t Base;
   tag_t Tag = __bb_lookup(Ptr, &Base);
-  XXH64_hash_t Hash = 0;
+  cov_idx_t Hash = 0;
 
   if (likely(Tag != kFuzzallocDefaultTag)) {
-    // Only compute the hash if not the default tag
+    // Compute the hash
+    const uintptr_t Use = (uintptr_t)__builtin_return_address(0);
+    Hash = (Tag - kFuzzallocDefaultTag) ^ Use;
 
-    uintptr_t Use = (uintptr_t)__builtin_return_address(0);
-    uint64_t Data[] = {Tag, Use};
-    Hash = XXH64(Data, sizeof(Data), kSeed);
 #ifdef _DEBUG
     fprintf(stderr,
-            "[datAFLow] hash(tag=0x%" PRIx16 ", use=%" PRIx64 ") -> %zu\n", Tag,
-            Use, Hash);
+            "[datAFLow] hash(tag=0x%" PRIx16 ", use=%" PRIx64 ") -> %" PRIxHash
+            "\n",
+            Tag, Use, Hash);
 #endif
   }
 
@@ -70,20 +69,18 @@ void __afl_hash_def_use(void *Ptr, size_t Size) {
 void __afl_hash_def_use_offset(void *Ptr, size_t Size) {
   uintptr_t Base;
   tag_t Tag = __bb_lookup(Ptr, &Base);
-  XXH64_hash_t Hash = 0;
+  cov_idx_t Hash = 0;
 
   if (likely(Tag != kFuzzallocDefaultTag)) {
-    // Only compute the hash if not the default tag
-
-    uintptr_t Use = (uintptr_t)__builtin_return_address(0);
-    ptrdiff_t Offset = (uintptr_t)Ptr - Base;
-    uint64_t Data[] = {Tag, Use, Offset};
-    Hash = XXH64(Data, sizeof(Data), kSeed);
+    // Compute the hash
+    const uintptr_t Use = (uintptr_t)__builtin_return_address(0);
+    const ptrdiff_t Offset = (uintptr_t)Ptr - Base;
+    Hash = (Tag - kFuzzallocDefaultTag) ^ (Use + Offset);
 
 #ifdef _DEBUG
     fprintf(stderr,
             "[datAFLow] hash(tag=0x%" PRIx16 ", use=%" PRIx64
-            ", offset=0x%" PRIu64 ") -> %zu\n",
+            ", offset=0x%" PRIu64 ") -> %" PRIxHash "\n",
             Tag, Use, Offset, Hash);
 #endif
   }
@@ -94,45 +91,22 @@ void __afl_hash_def_use_offset(void *Ptr, size_t Size) {
 void __afl_hash_def_use_value(void *Ptr, size_t Size) {
   uintptr_t Base;
   tag_t Tag = __bb_lookup(Ptr, &Base);
-  XXH64_hash_t Hash = 0;
+  cov_idx_t Hash = 0;
 
   if (likely(Tag != kFuzzallocDefaultTag)) {
-    // Setup a rolling hash for hashing the (variable-lengthed) variable value
+    // Compute the hash
+    const uintptr_t Use = (uintptr_t)__builtin_return_address(0);
+    const ptrdiff_t Offset = (uintptr_t)Ptr - Base;
 
-    // Initialize the hash state
-    static XXH64_state_t *State = NULL;
-    if (unlikely(!State)) {
-      State = XXH64_createState();
-      if (unlikely(!State)) {
-        abort();
-      }
+    Hash = (Tag - kFuzzallocDefaultTag) ^ (Use + Offset);
+    for (uint8_t *I = Ptr, *End = Ptr + Size; I < End; ++I) {
+      Hash ^= *I;
     }
-
-    // Reset hash state
-    if (unlikely(XXH64_reset(State, kSeed) == XXH_ERROR)) {
-      abort();
-    }
-
-    uintptr_t Use = (uintptr_t)__builtin_return_address(0);
-    ptrdiff_t Offset = (uintptr_t)Ptr - Base;
-    uint64_t Data[] = {Tag, Use, Offset};
-
-    if (unlikely(XXH64_update(State, Data, sizeof(Data)) == XXH_ERROR)) {
-      abort();
-    }
-
-    // Hash the value at the use site
-    if (unlikely(XXH64_update(State, Ptr, Size) == XXH_ERROR)) {
-      abort();
-    }
-
-    // Produce the digest
-    Hash = XXH64_digest(State);
 
 #ifdef _DEBUG
     fprintf(stderr,
             "[datAFLow] hash(tag=0x%" PRIx16 ", use=%" PRIx64
-            ", offset=0x%" PRIu64 ", obj=%p, size=%zu) -> %zu\n",
+            ", offset=0x%" PRIu64 ", obj=%p, size=%zu) -> %" PRIxHash "\n",
             Tag, Use, Offset, Ptr, Size, Hash);
 #endif
   }
