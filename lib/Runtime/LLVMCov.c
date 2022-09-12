68a4158cf37ef73459e9cfebb9d531e967033cef
Adrian Herrera <adrian.herrera02@gmail.com>
2022-09-12 14:14:42 +1000

    runtime: added new lib for llvm-cov

---
+++ lib/Runtime/LLVMCov.c
@@ -0,0 +1,39 @@
+//===-- LLVMCov.cpp - Clang source-based coverage runtime ---------*- C -*-===//
+///
+/// \file
+/// Runtime for fine-grained control of Clang's source-based coverage
+///
+//===----------------------------------------------------------------------===//
+
+#include <signal.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/time.h>
+
+int __llvm_profile_runtime = 0;
+void __llvm_profile_register_write_file_atexit(void);
+int __llvm_profile_write_file(void);
+
+static void handleTimeout(int Sig) {
+  __llvm_profile_write_file();
+}
+
+__attribute__((constructor)) static void initializeTimeout() {
+  char *Timeout = getenv("LLVM_PROFILE_TIMEOUT");
+  struct sigaction SA;
+  struct itimerval It;
+
+  if (Timeout) {
+    long T = atol(Timeout);
+    bzero(&SA, sizeof(struct sigaction));
+    SA.sa_handler = handleTimeout;
+    sigaction(SIGALRM, &SA, NULL);
+
+    bzero(&It, sizeof(struct itimerval));
+    It.it_value.tv_sec = T / 1000;
+    It.it_value.tv_usec = (T % 1000) * 1000;
+    setitimer(ITIMER_REAL, &It, NULL);
+  } else {
+    __llvm_profile_register_write_file_at_exit();
+  }
+}
